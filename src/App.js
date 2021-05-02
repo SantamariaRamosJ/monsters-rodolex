18b08c4411c85ae466dfe3d3526b3f87e3f8bec5
SantamariaRamosJ <77521543+SantamariaRamosJ@users.noreply.github.com>
2021-05-02 15:21:44 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+import { CardList } from './components/card-list.component';
+
+class App extends Component {
+  constructor() {
+    super(); 
+    this.state = {
+      monsters: []
+    };
+}
+
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({monsters: users }));
+}
+
+  render() {
+    return (
+      <div className="App">
+        <CardList name="Jorge">
+          {this.state.monsters.map(monsters => (
+            <h1 key={monsters.id}>{monsters.name} </h1>
+            ))}
+          </CardList>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/card-list.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './card-list.style.css';
+
+export const CardList = (props) => {
+    console.log(props);
+    return <div className='cardList'> {props.children} </div>;
+};
