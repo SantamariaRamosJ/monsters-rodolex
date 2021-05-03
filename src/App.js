0c5f502a03b8ea3cd79f3c1683fd803d745e4d23
SantamariaRamosJ <santamariaramosj@gmail.com>
2021-05-02 21:28:52 -0500

    new files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list.component';
+import { SearchBox } from './components/search-box.component';
 
 class App extends Component {
   constructor() {
     super(); 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
 }
 
@@ -16,11 +18,25 @@ componentDidMount() {
   .then(users => this.setState({monsters: users }));
 }
 
+handleChange = e => {
+  this.setState({ searchField: e.target.value });
+};
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monsters => 
+      monsters.name.toLowerCase().includes(searchField.toLocaleLowerCase()));
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}/>
-          
+        <h1 className='title'>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        /> 
+        <CardList 
+          monsters={filteredMonsters}
+        />
       </div>
     );
   }
--- src/components/card-components.jsx
+++ src/components/card-components.jsx
@@ -8,7 +8,7 @@ export const Card = props => (
             alt='monster'
             src={`https://robohash.org/${props.monsters.id}?set=set2&size=180x180`}
         />
-        <h1> {props.monsters.name} </h1>
+        <h1 className="monstersName"> {props.monsters.name} </h1>
         <p> {props.monsters.email} </p>
     </div>
 );
